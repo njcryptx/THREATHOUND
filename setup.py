d7464a19a0a5a622ca4c4021d574e6b9fbe36821
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2025-03-18 02:15:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+import os
+import sys
+import subprocess
+import shutil
+
+def main():
+    if os.geteuid() != 0:
+        print("Error: Requires sudo privileges", file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
